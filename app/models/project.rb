92f95483ec07b79b73cd08bfe75147658ac239a1
Sherman Kettner <skettner@uccs.edu>
2022-11-22 21:32:47 +0000

    commented out validates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+    #validates :title, presence: true
+    #validates :description, presence: true
 
 end
--- spec/controller/project_spec.rb
+++ spec/controller/project_spec.rb
@@ -1,19 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
-    RSpec.describe Project, type: :model do
-    # ...
-   
-    context "scopes tests" do
-         let(:params) { { title: "Title", description: "some description" } }
-         before(:each) do
-            Project.create(params)
-            Project.create(params)
-            Project.create(params)
-         end
+RSpec.describe ProjectsController, type: :controller do
+    context "GET #index" do
+        it "returns a success response" do
+            get :index
+            expect(response).to be_successful
+        end
+    end
 
-         it "should return all projects" do
-            expect(Project.count).to eq(3)
-         end
-         
-      end
-   end
+    context "Get #show" do
+        let!(:project) { Project.create(title: "Test title", description: "Test Description") }
+        it "returns a success response" do
+            get :show, params: { id: project }
+            expect(response).to be_successful
+        end
+    end
+end
